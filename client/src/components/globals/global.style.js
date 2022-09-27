7d93b6e4f330612147f42a473e9e2de2b6430883
Martin Teigen <martin@teigen.biz>
2022-09-27 12:45:20 +0200

    Import fonts

--- client/src/components/globals/global.style.js
+++ client/src/components/globals/global.style.js
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     background-color: ${standardColors.white1};
+    font-family: "inria-sans-light", serif ;
   }
 `;
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,5 +4,12 @@ import { App } from "./app";
 
 import "bulma/css/bulma.min.css";
 import "../public/css/menuBurger.css";
+import { FontStyles } from "./components/globals/fonts/fonts.style";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <div>
+    <FontStyles />
+    <App />
+  </div>,
+  document.getElementById("app")
+);
