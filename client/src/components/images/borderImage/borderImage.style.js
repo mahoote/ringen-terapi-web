1146561a895ae09314aa23f8ae8f8a4d3b79bab0
Martin Teigen <martin@teigen.biz>
2022-09-27 17:37:21 +0200

    Details

--- client/src/components/images/borderImage/borderImage.style.js
+++ client/src/components/images/borderImage/borderImage.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { BorderImage } from "./borderImage";
 
 const BorderImageStyled = styled(BorderImage)`
+  position: relative;
   background-color: ${(props) => props.backgroundColor};
   padding: ${(props) => props.padding};
 `;
