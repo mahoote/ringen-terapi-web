cf690920ea218ea7e5e0e28f4ac40ffcb7b8a45b
mahoote <martin@teigen.biz>
2022-10-13 17:21:34 +0200

    Refactor

--- client/src/components/containers/servicesPage/section01/servicesSectionOne.jsx
+++ client/src/components/containers/servicesPage/section01/servicesSectionOne.jsx
@@ -12,6 +12,19 @@ import {
 } from "../../../globals/fontSizes";
 import { ServicesSectionOneHeadlineStyled } from "./servicesSectionOne.style";
 import { standardColors } from "../../../../palettes/standardColors.style";
+const images = [
+  {
+    src: require("../../../../../public/images/people01.jpg"),
+    alt: data.servicesPage.section1.image1.description,
+    width: data.servicesPage.section1.image1.width,
+    widthMobile: data.servicesPage.section1.image1.widthMobile,
+    backgroundColor: standardColors.blue1,
+    offsetX: "-3em",
+    offsetY: "2em",
+    offsetXMobile: "-1em",
+    offsetYMobile: "-3em",
+  },
+];
 
 function ServicesSectionOneHeadline(props) {
   const { services } = props;
@@ -47,69 +60,17 @@ function ServicesSectionOneHeadline(props) {
   );
 }
 
-function ServicesSectionOneService(props) {
-  return (
-    <div className={props.className}>
-      <TextStyled
-        text={props.headline}
-        sizeDesktop={desktopBig}
-        sizeTablet={tabletBig}
-        sizeMobile={mobileBig}
-        textWeight={"bold"}
-      />
-      <TextStyled
-        className={"my-5"}
-        text={props.text}
-        sizeDesktop={desktopSmall}
-        sizeTablet={desktopSmall}
-        sizeMobile={desktopSmall}
-      />
-    </div>
-  );
-}
-
 function ServicesSectionOne(props) {
   const services = data.servicesPage.services;
-
-  const images = [
-    {
-      src: require("../../../../../public/images/people01.jpg"),
-      alt: data.servicesPage.section1.image1.description,
-      width: data.servicesPage.section1.image1.width,
-      widthMobile: data.servicesPage.section1.image1.widthMobile,
-      backgroundColor: standardColors.blue1,
-      offsetX: "-3em",
-      offsetY: "2em",
-      offsetXMobile: "-2em",
-      offsetYMobile: "-1em",
-    },
-    {
-      src: require("../../../../../public/images/people01.jpg"),
-      alt: services[0].image.description,
-      width: services[0].image.width,
-      widthMobile: services[0].image.widthMobile,
-      backgroundColor: standardColors.brown3,
-      offsetX: "0em",
-      offsetY: "2em",
-      offsetXMobile: "2em",
-      offsetYMobile: "1em",
-    },
-  ];
 
   return (
     <ImageTextSection
       className={props.className}
-      borderImagesHeight={"300px"}
+      borderImagesHeight={"220px"}
       textContentFirst={
         <ServicesSectionOneHeadlineStyled services={services} />
       }
-      images={[images[0], images[1]]}
-      textContentSecond={
-        <ServicesSectionOneService
-          headline={services[0].title}
-          text={services[1].about}
-        />
-      }
+      images={[images[0]]}
     />
   );
 }
--- client/src/pages/servicesPage/servicesPage.jsx
+++ client/src/pages/servicesPage/servicesPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { PageLayout } from "../../components/containers/pageLayout/pageLayout";
 import { ServicesSectionOne } from "../../components/containers/servicesPage/section01/servicesSectionOne";
+import { ServicesSectionTwo } from "../../components/containers/servicesPage/section02/servicesSectionTwo";
 
 function ServicesPage() {
   return (
     <PageLayout>
-      <ServicesSectionOne className={"mb-6"} />
+      <ServicesSectionOne />
+      <ServicesSectionTwo />
+      <div className={"mb-6"} />
     </PageLayout>
   );
 }
